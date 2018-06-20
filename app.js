425f8100fbd9220ab9200d3e5065897e800236fa
Jason Schmitt <schmitty890@gmail.com>
2018-06-20 13:39:00 -0400

    setup json array and vue binding

--- app.js
+++ app.js
@@ -1,7 +1,60 @@
-new Vue({
-  el: '',
-  data: {
+const media = [
+    {
+      title: 'A wonderful horrible life', 
+      description: "Technical skateboard video",
+      type: 'DVD',
+      contributor: 'PJ Ladd',
+      showDetail: false,
+    },
+    {
+      title: 'Team Fortress', 
+      description: "Push the payload for victory",
+      type: 'Games',
+      contributor: 'Valve',
+      showDetail: false
+    },
+    {
+      title: 'Kicker Issue 41', 
+      description: "Skateboard documentary",
+      type: 'DVD',
+      contributor: "Thrasher",
+      showDetail: false
+    },
+    {
+      title: 'Planet Earth', 
+      description: "A documentary about the planet earth",
+      type: 'tv shows',
+      contributor: 'National Geographic',
+      showDetail: false,
+    },
+    {
+      title: 'Titanic', 
+      description: "The boat sinks.",
+      type: 'DVD',
+      contributor: 'James Cameron',
+      showDetail: false,
+    },
+    {
+      title: 'Breaking Bad', 
+      description: "The blue stuff",
+      type: 'tv shows',
+      contributor: '',
+      showDetail: false,
+    },
+    {
+      title: 'Fortnite', 
+      description: "100 people on an island, fight to the death",
+      type: 'games',
+      contributor: 'Epic Games',
+      showDetail: false,
+    }
+  ];
 
+new Vue({
+  el: '#media-list',
+  data: {
+    title: 'Jasons public library',
+    meiaList: media
   },
   methods: {
 
